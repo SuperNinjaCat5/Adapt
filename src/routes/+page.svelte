dfdd766e40fa3bb83cbd58e19d811ccc0821f9bc
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-09-01 16:50:40 -0400

    Init repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>Adapt - Home</title>
+    <meta name="description" content="You ship a Ai, We ship Ai related rewards!" />
+
+    <link rel="stylesheet" href="/app.css">
+</svelte:head>
+
+<div class="h-screen w-screen overflow-hidden gradient-bg flex items-center justify-center">
+    <div class="container">
+        <h1 class="title">Adapt</h1>
+        <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    </div>
+</div>
