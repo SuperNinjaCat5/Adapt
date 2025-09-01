855b1d7a9fff6a08999d4fe213e0547ee536f76e
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-09-01 17:54:37 -0400

    Refactor home page layout and styles; update description and add RSVP button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <svelte:head>
     <title>Adapt - Home</title>
-    <meta name="description" content="You ship a Ai, We ship Ai related rewards!" />
+    <meta name="description" content="You ship a Ai Model, We ship Ai related rewards!" />
 
-    <link rel="stylesheet" href="/app.css">
+    <link rel="stylesheet" href="/css/app.css">
 </svelte:head>
 
-<div class="h-screen w-screen overflow-hidden gradient-bg flex items-center justify-center">
+<div class="h-screen w-screen overflow-hidden body-container gradient-bg flex">
     <div class="container">
         <h1 class="title">Adapt</h1>
-        <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+        <p class="subtitle">You ship a Ai Model, We ship Ai related rewards!</p>
+
+        <div class="circle-wrapper">
+            <p class="circle-container"></p>
+            <div class="center_xy">
+                <button class="btn-primary circle-btn">RSVP</button>
+            </div>
+        </div>
     </div>
 </div>
